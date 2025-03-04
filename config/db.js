4affcbac878a2338fa44f9e02f74a9b6510337af
Rajneesh Kumar Maurya <144912920+rajneeshkumar222@users.noreply.github.com>
2025-03-04 23:45:35 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB Connected Successfully!");
   } catch (error) {
-    console.error("MongoDB Connection Failed", error);
-    process.exit(1);
+    console.error("❌ MongoDB Connection Failed:", error.message);
+    process.exit(1); // Forcefully stop the app if DB connection fails
   }
 };
+
 module.exports = connectDB;
